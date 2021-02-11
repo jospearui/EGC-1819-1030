ce8df60552309fb3fc94cc710f4f782d5208e424
EGC-jospearui <jospearui@alum.us.es>
2021-02-11 21:28:32 +0000

    Version inicial despliegue travis y heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -106,12 +106,8 @@ WSGI_APPLICATION = 'decide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'decide',
-        'USER': 'decide',
-        'PASSWORD': 'decide',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -156,9 +152,10 @@ STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
-APIS = {}
 
-BASEURL="https://prepexam121212.herokuapp.com"
+BASEURL= "https://prepexam121212.herokuapp.com"
+
+APIS = {}
 
 try:
     from local_settings import *
